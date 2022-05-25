4a8c7d63de0c9b9f900d36add3249c0d98d89a6c
dushy98 <dushy98@gmail.com>
2022-05-25 16:53:40 +0530

    Added solution - LeetHub

---
+++ 5. Array Delete And Shift - GFG/5.-array-delete-and-shift.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+def deleteFromArray(arr,n,idx):
+    #code here
+    # shifting all element sleft by 1 till idx
+    for i in range(idx,n-1):
+        arr[i] = arr[i+1]
+    arr[n-1] = 0
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#CONTRIBUTED BY RavinderSinghPB
+if __name__ == '__main__':
+    tcs= int(input())
+    
+    for _ in range(tcs):
+        n=int(input())
+        idx=int(input())
+        
+        arr=[i+1 for i in range(n)]
+        
+        deleteFromArray(arr,n,idx)
+        
+        for e in arr:
+            print(e,end=' ')
+        print()
+        
+# } Driver Code Ends
