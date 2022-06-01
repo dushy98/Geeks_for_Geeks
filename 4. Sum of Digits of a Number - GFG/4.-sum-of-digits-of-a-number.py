cce72356230272b031e27f6803a4d837c849f64a
dushy98 <dushy98@gmail.com>
2022-06-01 16:41:47 +0530

    Added solution - LeetHub

---
+++ 4. Sum of Digits of a Number - GFG/4.-sum-of-digits-of-a-number.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def sumOfDigits(self, n):
+        '''
+        :param n: given number
+        :return: sum of digits of n.
+        '''
+        # code here
+        if n < 10:
+            return n
+            
+        return n%10 + self.sumOfDigits(n//10)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        print(Solution().sumOfDigits(n))
+# } Driver Code Ends
