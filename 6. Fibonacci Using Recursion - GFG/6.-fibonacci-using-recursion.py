b0e5b8ab9e64ea8148affdee24346eb631df2d2d
dushy98 <dushy98@gmail.com>
2022-06-01 16:48:43 +0530

    Added solution - LeetHub

---
+++ 6. Fibonacci Using Recursion - GFG/6.-fibonacci-using-recursion.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+class Solution:
+    def fibonacci(self,n):
+        #code here
+        if n < 3:
+            return 1
+        
+        return self.fibonacci(n-1) + self.fibonacci(n-2)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        ob=Solution()
+        print(ob.fibonacci(n))
+# } Driver Code Ends
