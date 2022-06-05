d8c99fc6c60a770aa55bbaf24cb9c6d3ba208fd4
dushy98 <dushy98@gmail.com>
2022-06-05 23:15:14 +0530

    Added solution - LeetHub

---
+++ 3. Count 1's in binary array - GFG/3.-count-1s-in-binary-array.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // Function to count number of Ones
+    // arr: input array 
+    // N: size of input array
+    int countOnes(int arr[], int N)
+    {
+        
+        // Your code here
+    int count = 0;
+    for (int i = 0; i < N; i++)
+        if( arr[i] == 1)
+            count++;
+        else if(arr[i] == 0)
+            break;
+
+    return count;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int *arr = new int[n]; 
+        for(int i = 0; i < n; i++)
+            cin>>arr[i];
+            
+        Solution ob;
+        cout <<ob.countOnes(arr, n)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
