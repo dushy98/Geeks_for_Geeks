9d4d79b28968a856f42ed235b9c172c858433305
Dushyant Pal <dushy98@gmail.com>
2022-06-15 13:14:17 +0530

    Added solution - LeetHub

---
+++ 6. Merge Sort - GFG/6.-merge-sort.cpp
@@ -0,0 +1,90 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int m, int r)
+    {
+         // Your code here
+        int size1 = m-l+1;
+        int size2 = r-m;
+        
+        int leftArr[size1], rightArr[size2];
+        
+        for(int itr = 0; itr < size1; itr++){
+            leftArr[itr] = arr[itr+l];
+        }
+        
+        for(int itr = 0; itr < size2; itr++){
+            rightArr[itr] = arr[itr+m+1];
+        }
+        
+        int itr1 = 0, itr2 = 0, itr3 = l;
+        
+        while(itr1 < size1 && itr2 < size2){
+            if(leftArr[itr1] <= rightArr[itr2]){
+                arr[itr3++] = leftArr[itr1++]; 
+            }else{
+                arr[itr3++] = rightArr[itr2++];
+            }
+        }
+        
+        while(itr1 < size1){
+            arr[itr3++] = leftArr[itr1++];
+        }
+        
+        while(itr2 < size2){
+            arr[itr3++] = rightArr[itr2++];
+        }
+    }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+         if(r > l){
+            int mid = (l+r)/2;
+            mergeSort(arr,l,mid);
+            mergeSort(arr,mid+1, r);
+            merge(arr,l,mid,r);
+        }
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+    
+    scanf("%d",&n);
+    int arr[n+1];
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.mergeSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}  // } Driver Code Ends
