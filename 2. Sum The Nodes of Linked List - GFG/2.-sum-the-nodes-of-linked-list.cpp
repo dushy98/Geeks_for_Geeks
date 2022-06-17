c2bc7fd0793e18f7bdcd32057be15df5a64877c3
Dushyant Pal <dushy98@gmail.com>
2022-06-17 20:56:18 +0530

    Added solution - LeetHub

---
+++ 2. Sum The Nodes of Linked List - GFG/2.-sum-the-nodes-of-linked-list.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+int sumOfElements(struct Node* head);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < n-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        cout << sumOfElements(head) << endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+//User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+*/
+
+int sumOfElements(Node *head){
+   //Your code here
+   int sum = 0;
+   Node *current = head;
+   while (current != NULL){
+       sum += current->data;
+       current = current->next;
+   }
+   return sum;
+}
