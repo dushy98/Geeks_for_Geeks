eabd6a76c1faf2f2892d4ed34026e519edc7798a
Dushyant Pal <dushy98@gmail.com>
2022-06-22 20:04:09 +0530

    Added solution - LeetHub

---
+++ 12. Digital Root - GFG/12.-digital-root.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <iostream>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    //Complete this function
+    int digitalRoot(int n)
+    {
+        //Your code here
+    if (n<10)
+        return n;
+    else
+        return digitalRoot(n/10 + n%10); 
+
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main() {
+	int T;
+	cin>>T;//taking testcases
+	while(T--)
+	{
+	    int n;
+	    cin>>n;//taking number n
+	    
+	    //calling digitalRoot() function
+	    Solution obj;
+	    cout<<obj.digitalRoot(n)<<endl;
+	    
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
