3185a16c542e73a267493aea754eb317c04699c2
Dushyant Pal <dushy98@gmail.com>
2022-07-03 22:36:38 +0530

    Added solution - LeetHub

---
+++ 10. Multiplication Under Modulo - GFG/10.-multiplication-under-modulo.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        int multiplicationUnderModulo(long long a,long long b)
+    {
+        //your code here
+        long M = 1000000007;
+        a = a % M;
+        b = b % M;
+        return (a * b) % M;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	int T;
+	
+	//taking testcases
+	cin>>T;
+	while(T--)
+	{
+	    long long a;
+	    long long b;
+	    
+	    //taking input a and b
+	    cin>>a>>b;
+	    Solution ob;
+	    //calling multiplicationUnderModulo() function
+	    cout<<ob.multiplicationUnderModulo(a,b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
